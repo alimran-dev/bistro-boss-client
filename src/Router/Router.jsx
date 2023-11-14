9d3f76091533fbb8357c8dd727615c278097d596
alimran-dev <alimran5air@gmail.com>
2023-11-14 18:27:14 +0600

    bistro boss project created

---
+++ src/Router/Router.jsx
@@ -0,0 +1,9 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <div>hello</div>
+    }
+])
+export default router;
